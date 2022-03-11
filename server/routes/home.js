b6bc377ed6c0e1c41b515b3c1fbe6bb24330215e
Kelly <damian.kelly@defra.gov.uk>
2022-03-11 10:38:40 +0000

    first commit

---
+++ server/routes/home.js
@@ -0,0 +1,12 @@
+const joi = require('@hapi/joi')
+
+module.exports = [{
+  method: 'GET',
+  path: '/',
+  handler: (request, h) => {
+    return h.view('home', {
+      title: 'Hello',
+      message: 'World'
+    })
+  }
+}]
