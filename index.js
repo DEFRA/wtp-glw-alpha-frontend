242672a09cd5e3014de2e1147a54f58d185320a4
z424brave <kelly.damian@btinternet.com>
2022-03-16 12:45:28 +0000

    Add console log message

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const createServer = require('./server')
+const os= require('os');
 
 createServer()
-  .then(server => server.start())
+  .then(server => {
+    server.start();
+    console.log(`Server: ${process.version} / ${os.platform} / ${os.type} / ${os.version} / ${os.release}`);
+  })
   .catch(err => {
     console.log(err)
     process.exit(1)
